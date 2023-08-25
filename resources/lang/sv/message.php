a21f8a064d19bc506621401699de4cf9db60a4ce
profstyle1 <46463523+profstyle1@users.noreply.github.com>
2023-08-25 16:43:48 +0300

    New translations message.php (Swedish)

--- resources/lang/sv/message.php
+++ resources/lang/sv/message.php
@@ -43,4 +43,9 @@ return [
     // Register
     'recaptcha_field_is_required' => 'reCaptcha-fält krävs.',
     'failed_to_validate_captcha' => 'Det gick inte att validera captcha.',
+
+    'found_in' => ':to not found in :source!',
+
+    'email_address_in_use' => 'This email address is in use. Please try a different email address!',
+    'error' => 'Something went wrong. Please contact your system administrator!'
 ];
