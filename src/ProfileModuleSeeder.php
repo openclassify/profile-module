353df453ccc57283afc471c41a7c750505382c9b
Mostafa Moradi <mostafa@visiosoft.com.tr>
2024-08-18 14:57:39 +0300

    4.0-mostafa

--- src/ProfileModuleSeeder.php
+++ src/ProfileModuleSeeder.php
@@ -2,16 +2,12 @@
 
 use Anomaly\Streams\Platform\Database\Seeder\Seeder;
 use Visiosoft\ProfileModule\Education\EducationSeeder;
-use Visiosoft\ProfileModule\Notifications\ProfileModuleNotificationsTemplateSeeder;
 use Visiosoft\ProfileModule\Seed\UsersFieldsSeeder;
 
 class ProfileModuleSeeder extends Seeder
 {
     public function run()
     {
-        //Notifications Template Seeder
-        $this->call(ProfileModuleNotificationsTemplateSeeder::class);
-
         // Users Fields Seeder
         $this->call(UsersFieldsSeeder::class);
 
